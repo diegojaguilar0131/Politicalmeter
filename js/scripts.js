fa86c86e8385d8fed329d37e1d446ed41e050942
Diego Aguilar and Michael Dunlap <diego+michael@>
2017-06-01 14:20:34 -0700

    add 3 sets of radio buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,1 @@
-$(document).ready(function() {
-  var age = parseInt(prompt("How old are you?"));
 
-  if (age >= 18) {
-    $('#wait').hide();
-    $('#voting-age').show();
-  }else {
-    $('#wait').hide();
-    $('#minor').show();
-  }
-});
