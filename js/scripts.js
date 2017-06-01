3953a2e9caeed75c89fc63bfec7392a89b249771
Diego Aguilar and Michael Dunlap <diego+michael@>
2017-06-01 15:50:15 -0700

    add functionality to form

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,34 @@
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    var firstInput = parseInt($("input:radio[name=first]:checked").val());
+    var secondInput = parseInt($("input:radio[name=second]:checked").val());
+    var thirdInput = parseInt($("input:radio[name=third]:checked").val());
 
+    var sum = function(number1, number2, number3) {
+      var sum = number1 + number2 + number3;
+      return sum;
+    };
+
+    sum(firstInput,secondInput,thirdInput);
+
+    alert (sum(firstInput,secondInput,thirdInput));
+
+    if (sum<=3) {
+      $("#leaning").text("Liberal");
+      $("#output").show();
+
+    } else if (sum > 3 && sum < 7){
+      $("#leaning").text("Moderate");
+      $("#output").show();
+
+    } else {
+      $("#leaning").text("Conservative");
+      $("#output").show();
+    };
+
+
+
+    event.preventDefault();
+
+  });
+});
